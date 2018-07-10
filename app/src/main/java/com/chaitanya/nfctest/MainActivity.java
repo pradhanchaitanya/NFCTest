51ec6d707cbbee483ec6694d067f74f46c039808
pradhan_chaitanya <chaitanya.pradhan1211@gmail.com>
2018-07-11 01:22:42 +0530

    test

--- app/src/main/java/com/chaitanya/nfctest/MainActivity.java
+++ app/src/main/java/com/chaitanya/nfctest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int x = 5;
+    int y = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
