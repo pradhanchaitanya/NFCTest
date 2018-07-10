418410a69a6d8bcb8e074e4d473995c9233c6aa1
pradhan_chaitanya <chaitanya.pradhan1211@gmail.com>
2018-07-11 01:20:52 +0530

    test

---
+++ app/src/main/java/com/chaitanya/nfctest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chaitanya.nfctest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int x = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
